afa706df8a31baf7acda7720c2bacb10478f1ecf
roksana-barna <roksanabarna1803@gmail.com>
2023-03-28 02:55:26 +0600

    shop

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+
+    return (
+        <div>
+            <div className="shop-container">
+                <div className="product-container">
+                    {
+                        products.map(product=><Product key={product.id}
+                        product={product}></Product>)
+                    }
+
+                </div>
+
+                <div className="cart-container">
+                    <h4>nnn</h4>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Shop;

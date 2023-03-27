adbf72b264fa0f2afaa359fd57bfc31cf62642d7
roksana-barna <roksanabarna1803@gmail.com>
2023-03-28 01:16:02 +0600

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+
+            <div className=''>
+                
+                <a href="/shop">shop</a>
+                <a href="/inventory">inventory</a>
+                <a href="/order">order</a>
+                <a href="/login">login</a>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
